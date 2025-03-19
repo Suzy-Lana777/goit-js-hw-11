67b16a241ea414a84fbf63b42f5e1254e71864dc
Suzy-Lana777 <133979206+Suzy-Lana777@users.noreply.github.com>
2025-03-19 11:20:33 +0000

    Deploying to gh-pages from @ Suzy-Lana777/goit-js-hw-11@7f07a41b0232e134805096de68dec120c0520459 🚀

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-import"./assets/vendor-BUiUdO2e.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&c(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();document.querySelector(".form"),document.querySelector(".ul-gallery"),document.querySelector(".button"),document.querySelector(".loader");
+import{a as c,i as u,S as f}from"./assets/vendor-h_xsmXee.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();c.defaults.baseURL="https://jsonplaceholder.typicode.com";function p(i){const l=new URLSearchParams({q:i,key:"49399736-5639b789053676a95c3f2e38d",image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${l}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}function d(i){const{webformatURL:l,largeImageURL:r,tags:o,likes:e,views:t,comments:a,downloads:n}=i;return` 
+        <li class="li-gallery">
+
+        <a class="li-gallery-a" href="${r}">
+        <img class="img" src="${l}" 
+        alt="${o}" 
+        />
+        </a>
+        <ul class="baner-info">
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Likes</p>
+            <p class="baner-info-li-text">${e}</p>
+          </li>
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Views</p>
+            <p class="baner-info-li-text">${t}</p>
+          </li>
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Comments</p>
+            <p class="baner-info-li-text">${a}</p>
+          </li>
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Downloads</p>
+            <p class="baner-info-li-text">${n}</p>
+          
+            </li>
+        </ul>
+        </li> `}const s={form:document.querySelector(".form"),ulGallery:document.querySelector(".ul-gallery"),button:document.querySelector(".button"),loader:document.querySelector(".loader")};s.form.addEventListener("submit",m);function m(i){s.loader.classList.toggle("visually-hidden"),i.preventDefault();const l=i.currentTarget.elements.user_request.value.trim();if(l===""){button.disabled=!0;return}p(l).then(r=>{if(r.hits.length===0){s.loader.classList.toggle("visually-hidden"),u.error({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"}),s.ulGallery.innerHTML="",s.form.reset();return}const o=r.hits.map(t=>d(t)).join("");s.ulGallery.innerHTML=o,s.loader.classList.toggle("visually-hidden"),s.form.reset(),new f(".ul-gallery a",{captionsData:"alt"}).refresh()}).catch(r=>{console.log(r)})}
 //# sourceMappingURL=index.js.map
